0a789c57aa3b67627c8b0a9c4b5001e2e3432178
MRX173 <ms8685500@gmail.com>
2024-09-30 06:59:35 +0300

    hello

--- Program.cs
+++ Program.cs
@@ -7,21 +7,21 @@ var server = new WebSocketServer("ws://0.0.0.0:3000");
 var connections = new List<IWebSocketConnection>();
 server.Start(ws =>
 {
-    ws.OnOpen = () =>
+  ws.OnOpen = () =>
+  {
+    connections.Add(ws);
+  };
+  ws.OnMessage = message =>
+  {
+    foreach (var cn in connections)
     {
-        connections.Add(ws);
-    };
-    ws.OnMessage = message =>
-    {
-        foreach (var cn in connections)
-        {
-            if (cn != ws)
-            {
+      if (cn != ws)
+      {
 
-                cn.Send(message);
-            }
+        cn.Send(message);
+      }
 
-        };
     };
+  };
 });
 WebApplication.CreateBuilder(args).Build().Run();
