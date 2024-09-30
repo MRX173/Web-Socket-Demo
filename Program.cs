d8df49e0aaec90a3ad30e3e8b1b3451c7d930fd4
MRX173 <ms8685500@gmail.com>
2024-09-30 06:51:01 +0300

    add project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Fleck;
+
+
+
+var server = new WebSocketServer("ws://0.0.0.0:3000");
+
+var connections = new List<IWebSocketConnection>();
+server.Start(ws =>
+{
+    ws.OnOpen = () =>
+    {
+        connections.Add(ws);
+    };
+    ws.OnMessage = message =>
+    {
+        foreach (var cn in connections)
+        {
+            if (cn != ws)
+            {
+
+                cn.Send(message);
+            }
+
+        };
+    };
+});
+WebApplication.CreateBuilder(args).Build().Run();
